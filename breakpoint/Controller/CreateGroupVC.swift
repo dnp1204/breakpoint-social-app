6533582a21da59d4fe85c62beb5e8b07b28303e2
dnp <doanhpham1995@gmail.com>
2018-03-20 18:55:20 -0400

    search for email

--- breakpoint/Controller/CreateGroupVC.swift
+++ breakpoint/Controller/CreateGroupVC.swift
@@ -16,11 +16,15 @@ class CreateGroupVC: UIViewController {
     @IBOutlet weak var groupMemeberLbl: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
+    var emailArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        emailSearchTextField.delegate = self
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +36,19 @@ class CreateGroupVC: UIViewController {
     }
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmai(forSearchQuery: emailSearchTextField.text!, handler: { (returnEmailArray) in
+                self.emailArray = returnEmailArray
+                self.tableView.reloadData()
+            })
+        }
     }
 }
 
@@ -42,15 +58,17 @@ extension CreateGroupVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return emailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
         
         let profileImage = UIImage(named: "defaultProfileImage")
-        cell.configureCell(profileImage: profileImage!, email: "test2@example.com", isSelected: true)
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
         
         return cell
     }
 }
+
+extension CreateGroupVC: UITextFieldDelegate {}
--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Firebase
 import FirebaseDatabase
+import FirebaseAuth
 
 let DB_BASE = Database.database().reference()
 
@@ -73,6 +74,23 @@ class DataService {
             }
             
             handler(messageArray)
+        }
+    }
+    
+    func getEmai(forSearchQuery query: String, handler: @escaping (_ emailArray: [String]) -> ()) {
+        var emailArray = [String]()
+        
+        REF_USERS.observe(.value) { (userSnapshot) in
+            guard let userSnapshot = userSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            
+            for user in userSnapshot {
+                let email = user.childSnapshot(forPath: "email").value as! String
+                if email.contains(query) && email != Auth.auth().currentUser?.email {
+                    emailArray.append(email)
+                }
+            }
+            
+            handler(emailArray)
         }
     }
     
