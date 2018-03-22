70374309e24d6feb201dda8dd7cbb6220d49c2a6
dnp <doanhpham1995@gmail.com>
2018-03-21 22:37:59 -0400

    post to group

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class GroupFeedVC: UIViewController {
 
@@ -17,16 +18,32 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var sendBtn: UIButton!
     
     var group: Group?
+    var groupMessages = [Message]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sendBtnView.bindToKeyBoard()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         groupTitleLbl.text = group?.groupTitle
+        
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllMessagesFor(desiredGroup: self.group!, handler: { (returnedGroupMessages) in
+                self.groupMessages = returnedGroupMessages
+                self.tableView.reloadData()
+                
+                if self.groupMessages.count > 0 {
+                    let indexPath = IndexPath(row: self.groupMessages.count - 1, section: 0)
+                    self.tableView.scrollToRow(at: indexPath, at: .none, animated: true)
+                }
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,7 +55,17 @@ class GroupFeedVC: UIViewController {
     }
     
     @IBAction func sendBtnWasPressed(_ sender: Any) {
-        
+        if messageTextField.text != "" {
+            messageTextField.isEnabled = false
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: messageTextField.text!, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: group?.key, sendComplete: { (complete, nil) in
+                if complete {
+                    self.messageTextField.text = ""
+                    self.messageTextField.isEnabled = true
+                    self.sendBtn.isEnabled = true
+                }
+            })
+        }
     }
 
     @IBAction func backBtnWasPressed(_ sender: Any) {
@@ -53,13 +80,16 @@ extension GroupFeedVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupMessages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupFeedCell") as! GroupFeedCell else { return UITableViewCell() }
-        
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupFeedCell", for: indexPath) as? GroupFeedCell else { return UITableViewCell() }
+        let message = groupMessages[indexPath.row]
+        DataService.instance.getUsername(forUID: message.senderId) { (email) in
+            cell.configureCell(profileImage: UIImage(named: "defaultProfileImage")!, email: email, content: message.content)
+        }
+        return cell
     }
 }
 
